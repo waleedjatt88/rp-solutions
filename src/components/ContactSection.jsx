45f70a51f4f512e986cf323342b631e5d998bdf0
unknown <sendtomadnan@gmail.com>
2025-10-02 17:48:48 +0500

    Add Toaster & mail.js for messge add time

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -87,7 +87,7 @@ export default function ContactMe() {
       {toast.show && (
         <div
           className={`fixed top-5 right-5 flex items-center gap-3 border rounded-lg shadow-md px-4 py-2 
-          bg-gray-600 border-gray-400 text-gray-200 transition-all duration-300 z-50`}
+          bg-gray-600 border-gray-400 text-gray-200 transition-all duration-600 z-50`}
         >
           {toast.type === "success" ? (
             <FaCheckCircle className="text-[#f05228] text-xl" />
