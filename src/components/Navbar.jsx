970b3fb6055a26e16b9420b716ea9fe91515ef5c
unknown <sendtomadnan@gmail.com>
2025-11-03 22:58:08 +0500

    make build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -167,7 +167,7 @@ const Header = () => {
         </div>
 
         <div className="md:hidden">
-          {/* Mobile Button */}
+          button
           <button className="text-gray-300 focus:outline-none">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
