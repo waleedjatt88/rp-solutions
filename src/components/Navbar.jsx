f071960fbc51bc2eafe3c3e26cf90ab308aab03b
unknown <sendtomadnan@gmail.com>
2025-11-04 11:39:40 +0500

    fix the size of large images mb's to kb's

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -166,14 +166,14 @@ const Header = () => {
           })}
         </div>
 
-        <div className="md:hidden">
-          button
+        {/* <div className="md:hidden">
+          
           <button className="text-gray-300 focus:outline-none">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
           </button>
-        </div>
+        </div> */}
       </nav>
     </header>
   );
